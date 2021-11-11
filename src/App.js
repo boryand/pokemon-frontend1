57410ff4b70b40f873026087edd1fda6e29dca02
boryand <boryand>
2021-11-11 12:26:20 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
+import {
+  BrowserRouter as Switch,
+  Route,
+  Link,
+  NavLink,
+} from "react-router-dom";
+import Names from "./components/nameslist";
 
 function App() {
+  const [info, setInfo] = useState([]);
+  // empty array is a truthy value !!
+  useEffect(() => {
+    fetch("https://bxb-project.herokuapp.com/pokemon")
+      .then((response) => {
+        if (response.ok) {
+          console.log(response);
+          return response.json();
+        }
+        throw new Error("sorry,not working");
+      })
+      .then((data) => {
+        // console.log(data);
+        setInfo(data);
+      })
+      .catch((err) => {
+        console.log("err");
+      });
+  }, []);
+
+  //
+
+  const filteredNames = info.map((pokemon) => pokemon.name.english);
+  console.log(filteredNames);
+
+
+  //  return(
+  //   { info && info.name.english.map((names) => {
+  //        <fragment>
+  //             <p>{names.name}</p>
+  //           </fragment>
+
+  //       });
+
+  //  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route path="/pokemon">{info.length > 0 && <Names info={info} />}
+        </Route>
+      </Switch>
+    </>
   );
 }
 
---
+++ src/components/nameslist.js
@@ -0,0 +1,24 @@
+import React   from 'react';
+
+import {useParams} from "react-router-dom"
+
+
+
+
+
+
+const Names = ( {info}) => {
+    console.log(info)
+
+
+    return (
+        <div>
+            </div>
+    )
+
+
+
+
+}
+
+export default Names;
